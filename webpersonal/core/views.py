ff7fa192440e978f065409603e5a1fb324afa2e3
vicendev <49656917+vicendev@users.noreply.github.com>
2019-05-02 07:44:53 -0400

    WebPersonalUdemy
    Proyecto de Curso Udemy

---
+++ webpersonal/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+<h1>Mi Web Personal</h1>
+<ul>
+    <li><a href ="/">Portada</a></li>
+    <li><a href ="about-me/">Acerca de</a></li>
+</ul>
+"""
+# Create your views here.
+def home(request):
+    return HttpResponse(html_base + """
+        <h2>Portada</h2>
+        <p>Esto es la portada</p>
+    """)
+
+def about(request):
+    return HttpResponse("<h1>Mi Web Personal</h1><h2>Acerca de</h2><p>Hola me llamo Vicente y soy programador</p>")
